c29044b6ca85070ea5734b948982ecfd52e28331
volttron <volttron@pnnl.gov>
2019-08-13 10:49:00 -0700

    Put code back to store aggregate demand per market.

--- pnnl/transactive_base/transactive/aggregator_base.py
+++ pnnl/transactive_base/transactive/aggregator_base.py
@@ -28,6 +28,7 @@ class Aggregator(TransactiveBase):
         self.consumer_market = dict.fromkeys(consumer_market_base_name, [])
         self.supplier_market = []
         self.supplier_market = ['_'.join([supplier_market_base_name, str(i)]) for i in range(len(self.market_number))]
+        self.aggregate_demand = [None]*self.market_number
         if consumer_market_base_name:
             for market_name in self.consumer_market:
                 self.consumer_market[market_name] = ['_'.join([market_name, str(i)]) for i in range(len(self.market_number))]
@@ -48,6 +49,7 @@ class Aggregator(TransactiveBase):
         if buyer_seller == BUYER:
             market_index = self.supplier_market.index(market_name)
             _log.debug("{} - received aggregated {} curve - {}".format(self.agent_name, market_name, agg_demand.points))
+            self.aggregate_demand[market_index] = agg_demand
             self.translate_aggregate_demand(agg_demand, market_index)
 
             if self.consumer_market:
@@ -94,8 +96,16 @@ class Aggregator(TransactiveBase):
     def create_supply_curve(self, clear_price, supply_market):
         index = self.supplier_market.index(supply_market)
         supply_curve = PolyLine()
-        min_quantity = self.aggregate_demand[index].min_x()*0.8
-        max_quantity = self.aggregate_demand[index].max_x()*1.2
+        try:
+            if self.aggregate_demand:
+                min_quantity = self.aggregate_demand[index].min_x()*0.8
+                max_quantity = self.aggregate_demand[index].max_x()*1.2
+            else:
+                min_quantity = 0.0
+                max_quantity = 10000.0
+        except:
+            min_quantity = 0.0
+            max_quantity = 10000.0
         supply_curve.add(Point(price=clear_price, quantity=min_quantity))
         supply_curve.add(Point(price=clear_price, quantity=max_quantity))
         return supply_curve
